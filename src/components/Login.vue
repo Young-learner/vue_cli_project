4df95371666e5ab2962b01ceae7f00a6b7a9baa0
Young-learner <name_tl@163.com>
2019-12-20 16:45:03 +0800

    login page

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -6,19 +6,20 @@
         <img src="../assets/logo.png" alt="">
       </div>
       <!--登录表单区域-->
-      <el-form label-width="0px" class="login_form">
+      <el-form label-width="0px" class="login_form" :model="loginForm"
+               :rules="loginFormRule" ref="loginFormRef">
         <!--用户名-->
-        <el-form-item>
-          <el-input prefix-icon="el-icon-search"></el-input>
+        <el-form-item prop="username">
+          <el-input prefix-icon="iconfont icon-user" v-model="loginForm.username"></el-input>
         </el-form-item>
         <!--密码-->
-        <el-form-item>
-          <el-input prefix-icon="el-icon-search"></el-input>
+        <el-form-item prop="password">
+          <el-input prefix-icon="iconfont icon-3702mima" v-model="loginForm.password" type="password"></el-input>
         </el-form-item>
         <!--按钮区-->
         <el-form-item class="btns">
-          <el-button type="primary">登录</el-button>
-          <el-button type="info">重置</el-button>
+          <el-button type="primary" @click="login">登录</el-button>
+          <el-button type="info" @click="resetLoginForm">重置</el-button>
         </el-form-item>
       </el-form>
     </div>
@@ -28,7 +29,48 @@
 
 <script>
 export default {
-
+  data () {
+    return {
+      // 这是登录表单绑定的数据对象
+      loginForm: {
+        username: 'admin',
+        password: '123456'
+      },
+      // 这是表单的验证规则对象
+      loginFormRule: {
+        // 验证用户名是否合法
+        username: [
+          { required: true, message: '请输入登录名称', trigger: 'blur' },
+          { min: 3, max: 10, message: '长度在 3 到 10 个字符', trigger: 'blur' }
+        ],
+        // 验证密码是否合法
+        password: [
+          { required: true, message: '请输入登录密码', trigger: 'blur' },
+          { min: 6, max: 15, message: '长度在 6 到 15 个字符', trigger: 'blur' }
+        ]
+      }
+    }
+  },
+  methods: {
+    // 点击重置按钮.重置登录表单
+    resetLoginForm () {
+      // console.log(this)
+      this.$refs.loginFormRef.resetFields()
+    },
+    login () {
+      this.$refs.loginFormRef.validate(async valid => {
+        if (!valid) return
+        const { data: res } = await this.$http.post('login', this.loginForm)
+        // console.log(res)
+        if (res.meta.status !== 200) return this.$message.error('登录失败')
+        this.$message.success('登录成功')
+        // 将登录信息保存在本地
+        window.sessionStorage.setItem('token', res.data.token)
+        // 登录后跳转到后台主页
+        this.$router.push('/home')
+      })
+    }
+  }
 }
 </script>
 
--- src/router/index.js
+++ src/router/index.js
@@ -20,4 +20,18 @@ const router = new VueRouter({
   routes
 })
 
+// 挂载路由导航守卫
+router.beforeEach((to, from, next) => {
+  // to 要访问的路由
+  // from 代表从哪个路由过来的
+  // next是一个函数表示放行
+  // next()放行
+  // next('/路由')强制跳转
+  if (to.path === '/login') return next()
+  // 获取token
+  const tokenStr = window.sessionStorage.getItem('token')
+  if (!tokenStr) return next('/login')
+  next()
+})
+
 export default router
