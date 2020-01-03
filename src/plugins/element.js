190bf95669ec6eb471ae30609e9a45689d9686bb
Young-learner <name_tl@163.com>
2020-01-03 15:09:57 +0800

    order page

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -5,7 +5,7 @@ import { Button, Form, FormItem, Input, Message,
   Row, Col, Table, TableColumn, Switch, Tooltip,
   Pagination, Dialog, MessageBox, Tag, Tree,
   Select, Option, Cascader, Alert, Tabs, TabPane,
-  Steps, Step, CheckboxGroup, Checkbox, Upload } from 'element-ui'
+  Steps, Step, CheckboxGroup, Checkbox, Upload, Timeline, TimelineItem} from 'element-ui'
 
 Vue.use(Button)
 Vue.use(Form)
@@ -43,4 +43,6 @@ Vue.use(Step)
 Vue.use(CheckboxGroup)
 Vue.use(Checkbox)
 Vue.use(Upload)
+Vue.use(Timeline)
+Vue.use(TimelineItem)
 Vue.prototype.$confirm = MessageBox.confirm
